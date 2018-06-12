9685353fd9f0f1067d998c1497afa5a34a50b82f
Sangwon Hong <qpakzk@gmail.com>
2018-06-13 02:29:20 +0900

    Implement Lab2
    * Cube moves by following Sphere.
    * Cylinder rotates inputting arrow keys ny an user.
    
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

---
+++ Lab2/Assets/Cube.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+    GameObject sphere;
+	// Use this for initialization
+	void Start () {
+        sphere = GameObject.Find("Sphere");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(sphere.transform.position.x, sphere.transform.position.y, sphere.transform.position.z - 3);
+	}
+}
---
+++ Lab2/Assets/Cylinder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cylinder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yRotation = transform.localEulerAngles.y;
+        float zRotation = transform.localEulerAngles.z;
+
+        yRotation += Input.GetAxis("Horizontal");
+        zRotation += Input.GetAxis("Vertical");
+
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, yRotation, zRotation);
+    }
+}
